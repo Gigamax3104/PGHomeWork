4bd0779dbfccb7282902b9d0ac8bacf78f1b99e5
エネゴリ もどき <gigamaxstaf@gmail.com>
2025-10-21 19:53:23 +0900

    mult関数を処理。

---
+++ mult.cpp
@@ -0,0 +1,18 @@
+#include	<iostream>
+using namespace std;
+
+static int mult(int& a, int& b);
+
+int main() {
+	int a = 0;
+	int b = 0;
+
+	cout << "2‚Â‚Ì”š‚ğ“ü—Í‚µ‚Ä‚­‚¾‚³‚¢B>" << flush;
+	cin >> a >> b;
+
+	cout << "Š|‚¯Z‚µ‚½Œ‹‰Ê‚Í" << mult(a, b) << "‚Å‚·B" << endl;
+}
+
+static int mult(int& a, int& b) {
+	return a * b;
+}
