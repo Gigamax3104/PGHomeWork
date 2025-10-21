56398665ff7c9dfa8e5fc5c0d125978931856bb9
エネゴリ もどき <gigamaxstaf@gmail.com>
2025-10-21 17:57:20 +0900

    プロジェクト ファイルを追加します。

---
+++ StrFunction.cpp
@@ -0,0 +1,42 @@
+#include	<iostream>
+#include	<cstring>
+using namespace std;
+
+const int MAX = 18;
+
+const char* str_match(const char* s1, const char* s2);
+
+int main() {
+	char s1[MAX];
+	char s2[MAX];
+
+	cout << "1”Ô–Ú:" << flush;
+	cin >> s1;
+
+	cout << "2”Ô–Ú:" << flush;
+	cin >> s2;
+
+	cout << (unsigned int)&s1 << endl;
+	cout << (unsigned int)str_match(s1, s2) << endl;
+}
+
+const char* str_match(const char* s1, const char* s2) {
+	static int idx;
+	int SL = strlen(s2);
+
+	for (int i = 0; *(s1 + i) != '\0'; i++) {
+		if (*(s1 + i) == *(s2 + idx)) {
+			idx++;
+		}
+		else {
+			idx = 0;
+		}
+
+		if (idx == SL) {
+			return s1 + ((i - SL) + 1);
+			break;
+		}
+	}
+
+	return nullptr;
+}
