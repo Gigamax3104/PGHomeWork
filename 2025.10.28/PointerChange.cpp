c47bde36f3dcf0e652ee048c13c0f6c12b65cc18
エネゴリ もどき <gigamaxstaf@gmail.com>
2025-11-11 12:16:08 +0900

    ポインタのポインタに変更。

--- 2025.10.28/PointerChange.cpp
+++ 2025.10.28/PointerChange.cpp
@@ -1,7 +1,7 @@
 #include	<iostream>
 using namespace std;
 
-static void swap_ptr(char*& pStr1,char*& pStr2); //ポインタを交換する際にポインタ参照を扱う。
+static void swap_ptr(char** pStr1,char** pStr2); //ポインタを交換する際にポインタ参照を扱う。
 
 int main() {
 	char str1[] = "abcd";
@@ -13,14 +13,20 @@ int main() {
 	cout << pStr1 << endl; // abcdと表示される
 	cout << pStr2 << endl; // ABCDと表示される
 
-	swap_ptr(pStr1, pStr2);
+	swap_ptr(&pStr1, &pStr2);
 
 	cout << pStr1 << endl; // ABCDと表示される
 	cout << pStr2 << endl; // abcdと表示される
 }
 
 static void swap_ptr(char** pStr1,char** pStr2) { //*&より、**の方が良い。
+	cout << "mainのpStr1:" << (size_t)*pStr1 << endl;
+	cout << "mainのpStr2:" << (size_t)*pStr2 << endl;
+
 	char* p = *pStr1;
 	*pStr1 = *pStr2;
 	*pStr2 = p;
+
+	cout << "mainのpStr1:" << (size_t)*pStr1 << endl;
+	cout << "mainのpStr2:" << (size_t)*pStr2 << endl;
 }
